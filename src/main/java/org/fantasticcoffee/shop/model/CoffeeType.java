1e21bc4d0026353def5d47479094f16e3d1e6efa
RazvanCristea <razvancristea1994@gmail.com>
2021-07-21 11:31:44 +0300

    Reorganized CoffeeType

--- src/main/java/org/fantasticcoffee/shop/model/CoffeeType.java
+++ src/main/java/org/fantasticcoffee/shop/model/CoffeeType.java
@@ -4,104 +4,58 @@ import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 import static java.util.Arrays.asList;
 
 @Getter
 public enum CoffeeType {
 
-    ESPRESSO("Espresso") {
-        @Override
-        public CoffeeRecipe getRecipe() {
+    ESPRESSO("Espresso",
+            CoffeeRecipe.withBaseIngredients(
+                    new ArrayList<>(asList(
+                            BaseIngredient.ESPRESSO_SHOT,
+                            BaseIngredient.ESPRESSO_SHOT)))),
 
-            List<BaseIngredient> baseIngredients = new ArrayList<>(asList(
-                    BaseIngredient.ESPRESSO_SHOT,
-                    BaseIngredient.ESPRESSO_SHOT));
+    MACHIATTO("Machiatto",
+            new CoffeeRecipe(
+                    new ArrayList<>(asList(
+                            BaseIngredient.ESPRESSO_SHOT,
+                            BaseIngredient.ESPRESSO_SHOT)), Collections.singletonList(ExtraIngredient.MILK_FOAM))),
 
-            return CoffeeRecipe.withBaseIngredients(baseIngredients);
-        }
-    },
+    CAFFEE_LATTE("Caffee Latte",
+            new CoffeeRecipe(
+                    new ArrayList<>(asList(
+                            BaseIngredient.ESPRESSO_SHOT,
+                            BaseIngredient.ESPRESSO_SHOT)),
+                    new ArrayList<>(asList(ExtraIngredient.STEAMED_MILK,
+                            ExtraIngredient.STEAMED_MILK,
+                            ExtraIngredient.MILK_FOAM)))),
 
-    MACHIATTO("Machiatto") {
-        @Override
-        public CoffeeRecipe getRecipe() {
+    CAPPUCCINO("Cappuccino",
+            new CoffeeRecipe(
+                    new ArrayList<>(Collections.singletonList(
+                            BaseIngredient.ESPRESSO_SHOT)),
+                    new ArrayList<>(asList(ExtraIngredient.STEAMED_MILK,
+                            ExtraIngredient.MILK_FOAM,
+                            ExtraIngredient.MILK_FOAM)))),
 
-            List<BaseIngredient> baseIngredients = new ArrayList<>(asList(
-                    BaseIngredient.ESPRESSO_SHOT,
-                    BaseIngredient.ESPRESSO_SHOT));
+    CAFFEE_MIEL("Caffee Miel",
+            new CoffeeRecipe(
+                    new ArrayList<>(asList(
+                            BaseIngredient.BLACK_COFFEE,
+                            BaseIngredient.BLACK_COFFEE)),
+                    new ArrayList<>(asList(ExtraIngredient.HONEY,
+                            ExtraIngredient.CINNAMON,
+                            ExtraIngredient.STEAMED_MILK)))),
 
-            List<ExtraIngredient> extraIngredients = new ArrayList<>();
-            extraIngredients.addAll(Collections.singletonList(ExtraIngredient.MILK_FOAM));
-
-            return new CoffeeRecipe(baseIngredients, extraIngredients);
-        }
-    },
-
-    CAFFEE_LATTE("Caffee Latte") {
-        @Override
-        public CoffeeRecipe getRecipe() {
-
-            List<BaseIngredient> baseIngredients = new ArrayList<>();
-            baseIngredients.addAll(asList(
-                    BaseIngredient.ESPRESSO_SHOT));
-
-            List<ExtraIngredient> extraIngredients = new ArrayList<>();
-            extraIngredients.addAll(asList(ExtraIngredient.STEAMED_MILK,
-                    ExtraIngredient.STEAMED_MILK,
-                    ExtraIngredient.MILK_FOAM));
-
-            return new CoffeeRecipe(baseIngredients, extraIngredients);
-        }
-    },
-
-    CAPPUCCINO("Cappuccino") {
-        @Override
-        public CoffeeRecipe getRecipe() {
-
-            List<BaseIngredient> baseIngredients = new ArrayList<>();
-            baseIngredients.addAll(Collections.singletonList(
-                    BaseIngredient.ESPRESSO_SHOT));
-
-            List<ExtraIngredient> extraIngredients = new ArrayList<>();
-            extraIngredients.addAll(asList(ExtraIngredient.STEAMED_MILK,
-                    ExtraIngredient.MILK_FOAM,
-                    ExtraIngredient.MILK_FOAM));
-
-            return new CoffeeRecipe(baseIngredients, extraIngredients);
-        }
-    },
-
-    CAFFEE_MIEL("Caffee Miel") {
-        @Override
-        public CoffeeRecipe getRecipe() {
-
-            List<BaseIngredient> baseIngredients = new ArrayList<>();
-            baseIngredients.addAll(asList(
-                    BaseIngredient.BLACK_COFFEE,
-                    BaseIngredient.BLACK_COFFEE));
-
-            List<ExtraIngredient> extraIngredients = new ArrayList<>();
-            extraIngredients.addAll(asList(ExtraIngredient.HONEY,
-                    ExtraIngredient.CINNAMON,
-                    ExtraIngredient.STEAMED_MILK));
-
-            return new CoffeeRecipe(baseIngredients, extraIngredients);
-        }
-    },
-
-    DEFAULT("Be Creative!") {
-        @Override
-        public CoffeeRecipe getRecipe() {
-            return new CoffeeRecipe();
-        }
-    };
+    DEFAULT("Be Creative!",
+            new CoffeeRecipe());
 
     private String name;
+    private CoffeeRecipe recipe;
 
-    public abstract CoffeeRecipe getRecipe();
-
-    CoffeeType(String name) {
+    CoffeeType(String name, CoffeeRecipe recipe) {
         this.name = name;
+        this.recipe = recipe;
     }
 }
