0ea9b9a642c33529e1661a91e3062833c019e4b3
RazvanCristea <razvancristea1994@gmail.com>
2021-08-08 14:54:48 +0300

    deleted redundant import

--- src/main/java/org/fantasticcoffee/shop/App.java
+++ src/main/java/org/fantasticcoffee/shop/App.java
@@ -7,7 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@EnableJpaAuditing
+//@EnableJpaAuditing
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
